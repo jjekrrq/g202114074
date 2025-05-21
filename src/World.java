98e0a801d0fb15fa34744c9e59ca5058b34802ee
Lee Seungchang <jjekrrq01@naver.com>
2025-05-21 09:40:56 +0900

    five

--- src/World.java
+++ src/World.java
@@ -2,5 +2,9 @@ public class World {
 
     public static void run() { 
         System.out.println("202114074"); 
-    } 
+    }
+
+    public static void run2() {
+        System.out.println("hello world");
+    }
 }
